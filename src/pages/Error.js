d41531898a28c854c882bee4884e0894c57cafba
Kenne Serge <kenne_serge@mail.ru>
2020-08-30 04:26:19 +0300

    creatt hero and child Banner component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
-  return (
-    <div>
-      <h1>Error page</h1>
-    </div>
-  );
+  return <Hero hero="defaultHero">
+    <Banner title="404" subtitle="pagenot found">
+      <Link to="/" className="btn-primary">
+        return home
+      </Link>
+    </Banner>
+  </Hero>;
 };
 
 export default Error;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom';
 const Home = () => {
-  return (
-    <div>
-      <h1>Home page</h1>
-    </div>
-  );
+  return <Hero hero="defaultHero">
+      <Banner 
+        title="Luxurious Rooms"
+        subtitle="deluxe rooms starting at $299"
+      >
+          <Link to="/rooms" className="btn-primary">
+              our Rooms
+          </Link>
+      </Banner>
+  </Hero>
 };
 
 export default Home;
