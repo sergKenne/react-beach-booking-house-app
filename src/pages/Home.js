250ad39b457fb9d7c9f0ea085c01caedff4eb048
Kenne Serge <kenne_serge@mail.ru>
2020-08-30 05:23:02 +0300

    services component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,23 @@ import React from 'react';
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import { Link } from 'react-router-dom';
+import Services from '../components/Services';
 const Home = () => {
-  return <Hero hero="defaultHero">
-      <Banner 
-        title="Luxurious Rooms"
-        subtitle="deluxe rooms starting at $299"
-      >
-          <Link to="/rooms" className="btn-primary">
-              our Rooms
-          </Link>
-      </Banner>
-  </Hero>
+  return (
+    <>  
+        <Hero hero="defaultHero">
+            <Banner 
+                title="Luxurious Rooms"
+                subtitle="deluxe rooms starting at $299"
+            >
+                <Link to="/rooms" className="btn-primary">
+                    our Rooms
+                </Link>
+            </Banner>
+        </Hero>
+        <Services/>
+    </>
+   ) 
 };
 
 export default Home;
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -3,15 +3,18 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 
+
 const Rooms = () => {
     return (
-      <Hero hero="roomsHero">
-        <Banner title="our rooms">
-          <Link to="/" className="btn-primary">
-            return home
-          </Link>
-        </Banner>
-      </Hero>
+      
+        <Hero hero="roomsHero">
+            <Banner title="our rooms">
+            <Link to="/" className="btn-primary">
+                return home
+            </Link>
+            </Banner>
+        </Hero>
+       
     );
 }
 
